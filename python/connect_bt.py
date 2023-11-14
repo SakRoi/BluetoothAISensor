cfc914d9863f69e0efb48b88937806941de913e7
SakRoi <roininensaku@gmail.com>
2023-11-14 15:59:35 +0200

    FEAT: add a way to connect to the bluetooth device

---
+++ python/connect_bt.py
@@ -0,0 +1,14 @@
+import asyncio
+from bleak import BleakClient
+
+import find_device
+
+address = str(asyncio.run(find_device.getKASA()))
+MODEL_NBR_UUID = "0x2902"
+
+async def main(address):
+    async with BleakClient(address) as client:
+        model_number = await client.read_gatt_char(MODEL_NBR_UUID)
+        print("Model Number: {0}".format("".join(map(chr, model_number))))
+
+asyncio.run(main(address))
--- python/find_device.py
+++ python/find_device.py
@@ -5,8 +5,9 @@ async def getKASA() -> str:
     devices = await BleakScanner.discover()
     for d in devices:
         if "KASA_LBS2" in str(d):
+            print(str(d))
             deviceName = str(d)
-            return deviceName[0:19]
+            return str(deviceName[0:17])
 
 
 if '__Main__':
