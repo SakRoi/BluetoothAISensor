e4bad01809534504bf5f48f9069c84f686f37b7c
SakRoi <roininensaku@gmail.com>
2023-11-16 11:48:14 +0200

    FEAT: integrate sql code with bluetooth connection, remove demo code

--- python/sql_write_base.py
+++ python/sql_write_base.py
@@ -1,20 +1,19 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-  host="800-callmemaybe",
-  user="dumbass",
-  password="uWishBitch",
-  database="measurements"
-)
-print(mydb)
 
-mycursor = mydb.cursor()
+def write_data(data: list) -> None:
+  mydb = mysql.connector.connect(
+    host="800-callmemaybe",
+    user="Dumbas",
+    password="youWishBish",
+    database="measurements"
+  )
+  
+  mycursor = mydb.cursor()
 
-writeInto = ("INSERT INTO rawdata (groupid, from_mac, to_mac, sensorvalue_a, sensorvalue_b, sensorvalue_c, sensorvalue_d, sensorvalue_f ) VALUES (%s, %s,%s, %s,%s, %s,%s, %s)")
-values = [(3, "nrf5340", "raspi", 1, 800, 85, 100, "KASA")]
+  writeInto = ("INSERT INTO rawdata (groupid, from_mac, to_mac, sensorvalue_a, sensorvalue_b, sensorvalue_c, sensorvalue_d, sensorvalue_f ) VALUES (%s, %s,%s, %s,%s, %s,%s, %s)")
+  values = [(3, "nrf5340", "raspi", data[3], data[0], data[1], data[2], "KASA")]
 
-mycursor.executemany(writeInto, values)
+  mycursor.executemany(writeInto, values)
 
-mydb.commit()
-
-print(mycursor.rowcount, "was inserted.")
+  mydb.commit()
