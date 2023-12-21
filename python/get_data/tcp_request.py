bd96567d5599f9f4234ff0bd581f2693bbcbddd0
SakRoi <roininensaku@gmail.com>
2023-12-21 13:12:16 +0200

    DOCS: update tcp_request.py documentation

--- python/get_data/tcp_request.py
+++ python/get_data/tcp_request.py
@@ -2,6 +2,8 @@ import requests
 import socket
 
 def write_data_to_csv(group: int):
+    '''Get the groups data from the API'''
+
     r = requests.get(f"http://172.20.241.9/luedataa_kannasta_groupid_csv.php?groupid={group}")
 
     with open("data.csv", "w", newline='') as data:
@@ -12,6 +14,8 @@ def write_data_to_csv(group: int):
             print(i, end = '')
 
 def write_data_to_txt(group: int):
+    '''Get the groups data from a specific port'''
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     groupWanted = f'{group}\n'
     groupWanted = groupWanted.encode()
@@ -19,12 +23,14 @@ def write_data_to_txt(group: int):
     s.sendall(groupWanted)
 
     chunks = []
+    #receive the data and decode it to utf-8
     while True:
         data = s.recv(1024)
         if len(data) == 0:
             break
         chunks.append(data.decode('utf-8'))
 
+    #Write the data to a .txt file
     with open("data.txt", "w", newline='') as data:
         for i in chunks:
             data.write(i)
@@ -33,5 +39,6 @@ def write_data_to_txt(group: int):
     s.close()
 
 if '__main__':
+    '''Write the wanted groups data as .txt and/or .csv file'''
     #write_data_to_txt(3)
     write_data_to_csv(3)
